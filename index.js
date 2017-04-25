eaa21b511d682171b9ff48361609ed8bbd1f5c50
Stephanie Dover <doverstephaniem@gmail.com>
2017-04-25 14:55:20 -0700

    split functions by page

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 'use strict';
 
-var currentUser;
-var users = [];
-function User (first, last, userName, favWine, favWinery, colorPref) {
-  this.first= first;
-  this.last= last;
-  this.userName = userName;
-  this.favWine = [];
-  this.favWinery = favWinery;
-  this.colorPref = colorPref;
-  this.bio = '';
-  this.id = Math.random()+''+Math.random();
-}
-
-function Comment(userName, comment, date) {
-  this.userName = userName;
-  this.comment = comment;
-  this.date = date;
-}
-
 function handleLoginSubmit(event) {
   event.preventDefault();
   var form = event.target;
@@ -45,7 +26,6 @@ function handleLoginSubmit(event) {
     form.reset();
   }
 }
-
 
 function handleUserGenerator (event){
   event.preventDefault();
@@ -74,76 +54,10 @@ function handleUserGenerator (event){
     console.log('something went wrong', error);
   }
 }
-function handleUserBio (event){
-  event.preventDefault();
-  var form = event.target;
-  var userBioUpdate = form.userBio.value;
-  this.bio = userBioUpdate;
-  form.reset();
-  try {
-    localStorage.users[currentUser].bio = JSON.stringify(users);
-  }
-  catch (error) {
-    console.log('failed to save bio update');
-  }
-}
-
-function check(currentUser) {
-  document.getElementById(currentUser).checked = true;
-}
-
-function uncheck() {
-  document.getElementById(currentUser).checked = false;
-}
-
-function inputchecker(id) {
-  var inputId = document.getElementById(id);
-
-}
-//users[currentUser];
-
-function loadUserData(){
-
-=======
-
-  var lastName = setAttribute;
-  var UserName = getElementById('');
-
-
-
-  var lastName = setAttribute.
-  var UserName = getElementById('');
-  var userGeneratorLastName = lastName.value;
-  var userGeneratorUserName = userName.value;
-  var userGeneratorFavWine = favWine.value;
-  var userGeneratorFavWinery = favWinery.value;
-  var userGeneratorColorPreference = colorPreference.value;
-
-  console.log(currentUser);
-}
 
 // loadUserData();
 
 // local storage object...!
-
-var testUser= new User('Michael','Axelson', 'Maxelson11',['merlot','malbec'], 'Chatuea St. Michelle','red');
-
-users.push(testUser);
-
-try {
-  users = JSON.parse(localStorage.users);
-}
-catch (error) {
-  console.log('something went wrong', error);
-}
-try {
-  currentUser = JSON.parse(localStorage.currentUser);
-}
-catch (error) {
-  console.log('current user not returned');
-}
-
-
 
 var loginLoad = document.getElementById('login-form');
 loginLoad.addEventListener('submit', handleLoginSubmit);
@@ -154,5 +68,3 @@ function profileReDirect() {
 // creates new users from form input
 var userGeneratorFormSubmit = document.getElementById('createUser');
 userGeneratorFormSubmit.addEventListener('submit', handleUserGenerator);
-var bioFormSubmit = document.getElementById('bio');
-bioFormSubmit.addEventListener('submit', handleUserBio);
