1de1cbd3b37b645db024ae52c6dcb3b5aa684f5d
Michael Axelson <axelmichael11@gmail.com>
2017-04-26 15:55:28 -0700

    generate questions and checkboxes

--- index.js
+++ index.js
@@ -27,28 +27,81 @@ function handleLoginSubmit(event) {
   }
 }
 
-wines= ['red','white'];
-
-wineList=['cab','merlot','malbec'];
-favorite = ['st.michelles','columbia Valley'];
 
 
- questionGenerator(wines, 'Whats your favorite wine?', 'favwine');
+var wines = ['red','white'];
+
+var wineList = ['cab','merlot','malbec'];
+
+var wineries = ['st.michelles','whatever winery'];
+
+var favoriteLocal = ['woodinville','columbia valley'];
+
+function checkBoxGenerator(list, listQuestion, questionName) {
+  var form = document.getElementById('createUser');
+  var header = document.createElement('h3');
+  header.textContent = listQuestion;
+  form.appendChild(header);
+  var ul = document.createElement('ul');
+  ul.setAttribute('id', 'question');
+  form.appendChild(ul);
+  for (var i = 0; i < list.length; i++) {
+    var li = document.createElement('li');
+    var checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('name', questionName);
+    checkbox.setAttribute('id', list[i]);
+    var label = document.createElement('label');
+    label.setAttribute('for', questionName);
+    label.textContent = list[i];
+    ul.appendChild(li);
+    li.appendChild(checkbox);
+    li.appendChild(label);
+  }
+
+}
+
+
+
+
+checkBoxGenerator(wines, 'Whats your favorite wine color?', 'favwine');
+checkBoxGenerator(wineList, 'What kinds of wines do you like to drink?', 'wineTypes');
+checkBoxGenerator(favoriteLocal, 'What are your favorite wineries??', 'favWineries');
+
+
+var question = 'question'
+var inputs = document.getElementById(question).getElementsByTagName('input');
+console.log(inputs.length);
+
+
+
+function experimentalUserGenerator(event) {
+  event.preventDefault();
+  var form = event.target;
+
+  var generatedUser = new User(profilePic, first, last, userName, wineColor, favWine, favWinery);
+}
 
 
 function handleUserGenerator (event){
   event.preventDefault();
   var form = event.target;
-  for (var i=0; i<wines.length; )
+
+  var generatedUser = new User(userGeneratorProfilePic, userGeneratorFirstName, userGeneratorLastName, userGeneratorUserName, userGeneratorFavWine, userGeneratorFavWinery);
+
   var userGeneratorFirstName = form.firstName.value;
   var userGeneratorLastName = form.lastName.value;
   var userGeneratorUserName = form.userName.value;
+  var inputs = document.getElementById(questionName).getElementsByTagName('input');
+
+  for (var i=0; i<users.length; ) {
+
+  if (input[i].checked)
   var userGeneratorFavWine = form.favWine.checked;
   var userGeneratorFavWinery = form.favWinery.checked;
   var userGeneratorProfilePic = form.profilePic.value;
   var userGeneratorWineColor = form.wineColor.value;
 
-  var generatedUser = new User(userGeneratorProfilePic, userGeneratorFirstName, userGeneratorLastName, userGeneratorUserName, userGeneratorFavWine, userGeneratorFavWinery);
 
   users.push(generatedUser);
   console.log(generatedUser);
@@ -62,6 +115,7 @@ function handleUserGenerator (event){
   }
   catch (error) {
     console.log('something went wrong', error);
+    }
   }
 }
 
