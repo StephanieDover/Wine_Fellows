fcec603137e59c08163ae479af57d2383ff2c2aa
Stephanie Dover <doverstephaniem@gmail.com>
2017-04-27 13:01:50 -0700

    added viewprofile page with html and some js

--- viewprofile.js
+++ viewprofile.js
@@ -1,1 +1,26 @@
 'use strict';
+var date = new Date();
+var n = date.toDateString();
+var time = date.toLocaleTimeString();
+var userProfileIndex = 1;
+// console.log(n + ' ' + time);
+//
+// var commentTime = document.getElementById('visiter-comments');
+// commentTime.textContent = n + ' ' + time;
+function profilePic() {
+  var image = document.getElementById('view-user-profile-pic');
+  image.src = users[userProfileIndex].profilePic;
+}
+profilePic();
+
+function profileName() {
+
+}
+
+function logoutHandler() {
+  localStorage.removeItem(currentUserIndex);
+  window.location.replace('index.html')
+}
+
+var logout = document.getElementById('logout');
+logout.addEventListener('submit', logoutHandler);
